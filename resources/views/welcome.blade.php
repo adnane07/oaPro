c4fc89cfcae60d1456eaee56838e057645a4d882
hafsa <hafsaelalami00@gmail.com>
2022-05-18 18:19:18 +0100

    HAFSA COMMIT

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
-      <h1> welcome page 1</h1>
+      <h1> welcome page CC 1</h1>
         </div>
 
     @endsection
